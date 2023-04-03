79b7cae43804ce0f1c24b1cde8f8519fcf92d6f9
Vladyslav Deda <Vladyslav.Deda@cantire.com>
2023-04-03 18:47:35 +0300

    add debounce for search city network call

--- presentation/src/main/java/com/example/presentation/location/LocationViewModel.kt
+++ presentation/src/main/java/com/example/presentation/location/LocationViewModel.kt
@@ -8,10 +8,12 @@ import com.example.domain.search.usecase.SearchCityUseCase
 import com.example.presentation.base.BaseViewModel
 import com.example.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class LocationViewModel @Inject constructor(
     private val searchCityUseCase: SearchCityUseCase,
@@ -22,10 +24,22 @@ class LocationViewModel @Inject constructor(
     private val _listOfHintsDtoState = MutableStateFlow<Resource<ListOfHintsDto>>(Resource.Error())
     val listOfHintsDtoState: StateFlow<Resource<ListOfHintsDto>> = _listOfHintsDtoState
 
+    private val _query = MutableSharedFlow<String>()
+
+    init {
+        viewModelScope.launch {
+            _query
+                .debounce(500)
+                .collectLatest {
+                    _listOfHintsDtoState.value = Resource.Loading()
+                    _listOfHintsDtoState.value = searchCityUseCase(it)
+                }
+        }
+    }
+
     fun searchCity(query: String) {
         viewModelScope.launch {
-            _listOfHintsDtoState.value = Resource.Loading()
-            _listOfHintsDtoState.value = searchCityUseCase(query)
+            _query.emit(query)
         }
     }
 
